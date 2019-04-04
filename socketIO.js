80c057adc5c8adcc05c517eba7a084ff8ac072d5
seonjin <akqmfspt07@naver.com>
2019-04-04 23:40:54 +0900

    socket.io name space add

--- socketIO.js
+++ socketIO.js
@@ -1,7 +1,58 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
 
 module.exports = (server) => {
   const io = SocketIO(server, { path: '/socket.io' });
+  // 네임 스페이스(실시간 데이터가 전달될 주소를 구별할 수 있음).
+  // io.of('/') -> 기본네임스페이스,
+  const room = io.of('/room');
+  const chat = io.of('/chat');
+
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
+    socket.on('disconnect', () => {
+      console.log('room 네임스페이스 접속 해제');
+    });
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스에 접속');
+    const req = socket.request;
+    const { headers: { referer } } = req;
+    // room/awefdsijfds (req.headers.referer) roomId 가져오기
+    const roomId = referer
+      .split('/')[referer.split('/').legnth - 1]
+      .replace(/\?.+/, '');
+    // socket.join(룸ID)
+    // socket.to(룸ID).emit()
+    // socket.leave(룸ID)
+    socket.join(roomId); // room 접속
+    socket.to(roomId).emit('join', { // 해당 room에만 event emit
+      user: 'system',
+      chat: `${req.session.color} 님이 입장하셨습니다.`,
+    });
+    socket.on('disconnect', () => {
+      console.log('chat 네임스페이스 접속 해제');
+      socket.leave(roomId); // room 나가기
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+      if (userCount === 0) {
+        axios.delete(`http://localhost:8085/room/${roomId}`)
+          .then(() => {
+            console.log('방 제거 요청 성공');
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color}님이 퇴장하셨습니다.`,
+        });
+      }
+    });
+  });
+
   io.on('connection', (socket) => {
     const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
