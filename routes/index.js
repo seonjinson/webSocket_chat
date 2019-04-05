a2b7c21170c44a697624f14af5420b8170b6c2da
seonjinson <akqmfspt07@naver.com>
2019-04-05 16:55:36 +0900

    room creation router add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 const express = require('express');
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  try {
+    const rooms = await Room.find({});
+    res.render('main', {
+      rooms,
+      title: 'GIF채팅방',
+      error: req.flash('roomError'),
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 채팅방 생성 router
+router.get('/room', (req, res, next) => {
+  res.render('room', { title: 'GIF 채팅방 생성' });
+});
+
+router.post('/room', async (req, res, next) => {
+  try {
+    const rooms = new Room({
+      title: req.body.title,
+      max: req.body.max,
+      owner: req.session.color,
+      password: req.body.password,
+    });
+    const newRoom = await rooms.save();
+    const io = req.app.get('io');
+    io.of('/room').emit('newRoom', newRoom);
+    res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 채팅방 들어가는 router
+router.get('/room/:id', async (req, res, next) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    const io = req.app.get('io');
+    if (!room) {
+      req.flash('roomError', '존재하지 않는 방입니다.');
+      return res.redirect('/');
+    }
+    if (room.password && room.password !== req.query.password) {
+      req.flash('roomError', '비밀번호가 틀렸습니다.');
+      return res.redirect('/');
+    }
+    const { rooms } = io.of('/chat').adapter; // adapter안에 방 정보.
+    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+      req.flash('roomError', '허용인원초과');
+      return res.redirect('/');
+    }
+    return res.render('chat', {
+      room,
+      title: room.title,
+      chats: [],
+      user: req.session.color,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
